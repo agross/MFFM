f24117f755ff1b8c82ea985f4519e7a81f81d05f
Alexander Groß <agross@therightstuff.de>
2025-02-18 19:28:04 +0100

    Work around button not having Command

--- src/Mffm/Core/ControlBindings/ButtonBinding.cs
+++ src/Mffm/Core/ControlBindings/ButtonBinding.cs
@@ -1,16 +1,41 @@
 ﻿using Mffm.Contracts;
+using System.Windows.Input;
 
 namespace Mffm.Core.ControlBindings;
 
 internal class ButtonBinding : IControlBinding
 {
-    public bool Bind(Control control, IFormModel formModel)
+  public bool Bind(Control control, IFormModel formModel)
+  {
+    if (control is not Button button) { return false; }
+
+    //button.DataBindings.Add(new Binding(nameof(button.CommandParameter), formModel, null, true, DataSourceUpdateMode.Never));
+    //button.DataBindings.Add(new Binding(nameof(button.Command), formModel, control.Name, true, DataSourceUpdateMode.OnPropertyChanged));
+
+    var command = formModel.GetType().GetProperty(control.Name).GetValue(formModel) as ICommand;
+
+    EventHandler click = (sender, args) => command.Execute(formModel);
+    button.Click += click;
+
+    EventHandler canExecute = (sender, args) => button.Enabled = command.CanExecute(formModel);
+    command.CanExecuteChanged += canExecute;
+
+    var form = button.FindForm();
+
+    EventHandler formShown = (sender, args) => canExecute(sender, args);
+    form.Shown += formShown;
+
+    FormClosedEventHandler formClosed = null!;
+    formClosed = (sender, args) =>
     {
-        if (control is not Button button) { return false; }
+      button.Click -= click;
+      command.CanExecuteChanged -= canExecute;
+      form.Shown -= formShown;
 
-        button.DataBindings.Add(new Binding(nameof(button.CommandParameter), formModel, null, true, DataSourceUpdateMode.Never));
-        button.DataBindings.Add(new Binding(nameof(button.Command), formModel, control.Name, true, DataSourceUpdateMode.OnPropertyChanged));
+      form.FormClosed -= formClosed;
+    };
 
-        return true;
-    }
+    form.FormClosed += formClosed;
+    return true;
+  }
 }
